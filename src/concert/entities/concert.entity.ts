acbe04f2949566734738931844a0aaffd8ac6151
yooseungyeop <yooseungyub@naver.com>
2024-07-04 18:00:08 +0900

    ✨ update /concert 엔티티 createconcert디티오생성

--- src/concert/entities/concert.entity.ts
+++ src/concert/entities/concert.entity.ts
@@ -1,1 +1,53 @@
-export class Concert {}
+import {
+  Column,
+  Entity,
+  Index,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { Category } from '../types/concertCategory.type';
+
+@Index('title', ['title'], { unique: true })
+@Entity({
+  name: 'concerts',
+})
+export class Concert {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'int', unique: true, nullable: false })
+  admin_id: number;
+
+  @Column({ type: 'varchar', unique: true, nullable: false })
+  title: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  content: string;
+
+  @Column({
+    type: 'enum',
+    enum: Category,
+    nullable: false,
+  })
+  category: Category;
+
+  @Column({ type: 'varchar', nullable: false })
+  location: string;
+
+  @Column({ type: 'int', default: 30000, nullable: false })
+  price: number;
+
+  @Column({ type: 'varchar', nullable: false })
+  image: string;
+
+  @Column({ type: 'int', default: 200, nullable: false })
+  seat_count: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
---
+++ src/concert/types/concertCategory.type.ts
@@ -0,0 +1,5 @@
+export enum Category {
+  Musical,
+  Classic,
+  Sports,
+}
